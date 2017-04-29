787b46e2ee0bc5436e723f47c905751f7539c42a
Gaurav Joshi <gaurav.joshi@gmail.com>
2017-04-29 10:59:12 +0530

    saving changes

--- src/algos/iq/QueueUsingStacks.java
+++ src/algos/iq/QueueUsingStacks.java
@@ -4,23 +4,23 @@ import edu.princeton.cs.algs4.Stack;
 
 public class QueueUsingStacks<T> {
 
-	private Stack<T> inbox = new Stack<>();
-	private Stack<T> outbox = new Stack<>();
+    private Stack<T> inbox = new Stack<>();
+    private Stack<T> outbox = new Stack<>();
 
-	public void enqueue(T element) {
-		inbox.push(element);
-	}
+    public void enqueue(T element) {
+        inbox.push(element);
+    }
 
-	public T dequeue() {
-		if (outbox.isEmpty()) {
-			while (!inbox.isEmpty())
-				outbox.push(inbox.pop());
-		}
-		return outbox.pop();
-	}
+    public T dequeue() {
+        if (outbox.isEmpty()) {
+            while (!inbox.isEmpty())
+                outbox.push(inbox.pop());
+        }
+        return outbox.pop();
+    }
 
-	public int size() {
-		return inbox.size() + outbox.size();
-	}
+    public int size() {
+        return inbox.size() + outbox.size();
+    }
 
 }
